e39a38266944b34078826b613947e96c0428056a
Fabian Kilpert <fkilpert@gmail.com>
2025-08-12 15:14:19 +0200

    start snakemake with apptainer

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
-snakemake -c 16 --use-conda -p --rerun-incomplete ## --profile humgen
+## snakemake -c 16 --use-conda -p --rerun-incomplete ## --profile humgen
+snakemake -c 16 --use-conda -p --rerun-incomplete --software-deployment-method apptainer
