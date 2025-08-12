16291305974da1e4f5c2d929c8de1d0adb5d715a
Fabian Kilpert <fkilpert@gmail.com>
2025-08-12 15:50:06 +0200

    add apptainer-args

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
 ## snakemake -c 16 --use-conda -p --rerun-incomplete ## --profile humgen
-snakemake -c 16 --use-conda -p --rerun-incomplete --software-deployment-method apptainer
+## snakemake -c 16 --use-conda -p --rerun-incomplete --software-deployment-method apptainer
+snakemake -c 16 --use-conda -p --rerun-incomplete --sdm "apptainer" --apptainer-args="--cleanenv" # to stop environment variables from being passed to the container
