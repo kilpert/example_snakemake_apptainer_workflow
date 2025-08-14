b013e731806dce79a100bfe214fe2f59e80fd38c
Fabian Kilpert <fkilpert@gmail.com>
2025-08-14 07:37:05 +0000

    run profile depending on hostname

--- run.sh
+++ run.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 
-## snakemake -c 16 --use-conda -p --rerun-incomplete ## --profile humgen
-## snakemake -c 16 --use-conda -p --rerun-incomplete --software-deployment-method apptainer
-snakemake -c 16 --use-conda -p --rerun-incomplete --sdm "apptainer" --apptainer-args="--cleanenv" # to stop environment variables from being passed to the container
+
+cmd="snakemake -c 16 --use-conda -p --rerun-incomplete --sdm apptainer --apptainer-args='--cleanenv'" # to stop environment variables from being passed to the container
+
+
+if [[ $(hostname) =~ "ikim" ]]; then
+    cmd=$(echo "$cmd --profile humgen")
+fi
+
+
+echo "$cmd"
+eval "$cmd"
