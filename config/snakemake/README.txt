e4a914727dc10d84b62149ad8b5a79c6a6926760
Fabian Kilpert <fkilpert@gmail.com>
2025-11-05 16:22:32 +0100

    add profile_for_apptainer

---
+++ config/snakemake/README.txt
@@ -0,0 +1,1 @@
+This folder contains Snakemake profiles.
--- run.sh
+++ run.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 
 
-cmd="snakemake -c 16 --use-conda -p --rerun-incomplete --sdm apptainer --apptainer-args='--cleanenv'" # to stop environment variables from being passed to the container
+## cmd="snakemake -c 16 --use-conda -p --rerun-incomplete"
+cmd="snakemake -c 16 -p --rerun-incomplete --sdm apptainer --apptainer-args='--cleanenv' --profile config/snakemake/profile_for_apptainer/" # --cleanenv: to stop environment variables from being passed to the container
 
 
-if [[ $(hostname) =~ "ikim" ]]; then
-    cmd=$(echo "$cmd --profile humgen")
-fi
+## if [[ $(hostname) =~ "ikim" ]]; then
+##     cmd=$(echo "$cmd --profile config/snakemake/profile_for_apptainer/")
+## fi
 
 
 echo "$cmd"
